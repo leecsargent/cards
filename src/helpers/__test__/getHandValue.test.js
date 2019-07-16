fe89ba4a93b013753f930937e3333e68a31d3fd1
Lee Sargent <leecsargent@gmail.com>
2019-07-15 21:30:28 -0400

    getHighestValue with aces as 1 or 10

--- src/helpers/__test__/getHandValue.test.js
+++ src/helpers/__test__/getHandValue.test.js
@@ -1,4 +1,11 @@
-import getHandValue from '../getHandValue';
+import getHandValue, {
+  nonAces,
+  isAce,
+  isNotAce,
+  valueOfNonAces,
+  highestAceValue,
+  getHighestValue,
+} from '../getHandValue';
 
 describe('getHandValue helper', () => {
   it('returns the sum of the blackjack values of all cards in a hand', () => {
@@ -8,5 +15,73 @@ describe('getHandValue helper', () => {
   });
   it('returns 0 for an empty list', () => {
     expect(getHandValue([])).toBe(0);
+  });
+});
+describe('nonAces', () => {
+  it('returns the cards that are not aces', () => {
+    expect(nonAces([{ value: '2' }, { value: 'ACE' }])).toEqual([
+      { value: '2' },
+    ]);
+  });
+});
+
+describe('isAce', () => {
+  it('returns true if value is ACE', () => {
+    expect(isAce({ value: 'ACE' })).toBe(true);
+  });
+  it('returns false if value not ACE', () => {
+    expect(isAce({ value: '2' })).toBe(false);
+  });
+});
+
+describe('isNotAce', () => {
+  it('returns true if value is not ACE', () => {
+    expect(isNotAce({ value: '2' })).toBe(true);
+  });
+  it('returns false if value is ACE', () => {
+    expect(isNotAce({ value: 'ACE' })).toBe(false);
+  });
+});
+
+describe('value of non-aces', () => {
+  it('returns sum of blackjack values of non-ace cards', () => {
+    expect(
+      valueOfNonAces([
+        { blackjackValue: 9, value: '9' },
+        { blackjackValue: 7, value: '7' },
+        { blackjackValue: 11, value: 'ACE' },
+      ]),
+    ).toBe(16);
+  });
+});
+
+describe('highestAceValue', () => {
+  it('returns the highest possible total given a previous total and a number of Aces', () => {
+    expect(highestAceValue(1, 1)).toBe(11);
+    expect(highestAceValue(2, 1)).toBe(21);
+    expect(highestAceValue(3, 1)).toBe(22);
+  });
+});
+
+describe('getHighestValue', () => {
+  it('returns the highest possible value of a hand of cards', () => {
+    expect(
+      getHighestValue([
+        { blackjackValue: 9, value: '9' },
+        { blackjackValue: 7, value: '7' },
+        { value: 'ACE' },
+      ]),
+    ).toBe(17);
+    expect(
+      getHighestValue([
+        { blackjackValue: 2, value: '2' },
+        { value: 'ACE' },
+        { value: 'ACE' },
+      ]),
+    ).toBe(13);
+    expect(
+      getHighestValue([{ value: 'ACE' }, { value: 'ACE' }, { value: 'ACE' }]),
+    ).toBe(21);
+    expect(getHighestValue([{ blackjackValue: 9, value: '9' }])).toBe(9);
   });
 });
--- src/helpers/getHandValue.js
+++ src/helpers/getHandValue.js
@@ -1,5 +1,28 @@
-export default cards => {
-  return cards.reduce((acc, curr) => {
+export const isAce = card => card.value === 'ACE';
+
+export const isNotAce = card => !isAce(card);
+
+export const nonAces = cards => cards.filter(isNotAce);
+
+export const valueOfNonAces = cards =>
+  nonAces(cards).reduce((acc, curr) => {
     return acc + curr.blackjackValue;
   }, 0);
+
+export const highestAceValue = (aces, total = 0) => {
+  return Array(aces)
+    .fill()
+    .reduce(acc => {
+      return acc + 10 <= 21 ? acc + 10 : acc + 1;
+    }, total);
 };
+
+export const getHighestValue = cards =>
+  highestAceValue(cards.filter(isAce).length, valueOfNonAces(cards));
+
+export const value = cards =>
+  cards.reduce((acc, curr) => {
+    return acc + curr.blackjackValue;
+  }, 0);
+
+export default value;
